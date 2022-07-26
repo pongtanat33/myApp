c76b3057436d52e7c19c917ec49a24ea53a8a524
pongtanat33 <pongtanat33@gmail.com>
2022-07-26 14:38:34 +0700

    add new9

--- agapp/src/environments/environment.prod.ts
+++ agapp/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // apiUrl: 'http://192.168.0.132:1234/users',
   // apiUrl_todo: 'http://192.168.0.132:1234/todos',
-  apiUrl: 'http://172.23.6.183:1234/users',
-  apiUrl_todo: 'http://172.23.6.183:1234/todos',
+  apiUrl: 'http://167.99.70.176:1235/users',
+  apiUrl_todo: 'http://167.99.70.176:1235/todos',
 };
